3c7df7b13df1767dcc898322081a79d9b6a78e18
Matthias Beerens <beerens.matthias@hotmail.com>
2018-07-29 10:46:57 +0200

    Updated models

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,11 +1,3 @@
 export class Recipe {
-  public name: string;
-  public description: string;
-  public imagePath: string;
-
-  constructor(name: string, desc: string, imgUrl: string) {
-    this.name = name;
-    this.description = desc;
-    this.imagePath = imgUrl;
-  }
+  constructor(public name: string, public description: string, public imagePath: string) { }
 }
