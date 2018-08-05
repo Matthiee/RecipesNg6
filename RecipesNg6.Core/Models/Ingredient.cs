e70188a771572e5ba717e66a803cf4ef14957083
Matthias Beerens <beerens.matthias@hotmail.com>
2018-08-05 10:41:27 +0200

    Added the models

---
+++ RecipesNg6.Core/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipesNg6.Core.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Amount { get; set; }
+
+    }
+}
---
+++ RecipesNg6.Core/Models/Recipe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipesNg6.Core.Models
+{
+    public class Recipe
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string ImagePath { get; set; }
+        public string Description { get; set; }
+
+    }
+}
